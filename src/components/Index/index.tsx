3b0c7eb65c8fe5aa445ea5d3717f963d9566f75c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-03-26 16:53:57 -0300

    initial commit

---
+++ src/components/Index/index.tsx
@@ -0,0 +1,16 @@
+const Index = () => {
+    return (
+        <>
+            <h1>NextJS Tailwind Boilerplate</h1>
+            <ul>
+                <li>Eslint</li>
+                <li>Prettier</li>
+                <li>Babel</li>
+                <li>TypeScript</li>
+                <li>Tailwind UI</li>
+            </ul>
+        </>
+    )
+}
+
+export default Index
