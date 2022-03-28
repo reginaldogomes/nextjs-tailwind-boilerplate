29dc4f8fca2b7f3d08887567303f2fe86ca266e7
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-03-28 16:54:41 -0300

    tailwind config edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purge:['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+    purge:['./src/components/**/*.{js, ts, jsx, tsx}', './src/pages/**/*.{js, ts, jsx, tsx}'],
     content: [],
     theme: {
         extend: {},
