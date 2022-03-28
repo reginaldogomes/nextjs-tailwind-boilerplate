65a09c72783848c216db7b86b2027fe373e3ab8a
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-03-28 15:23:55 -0300

    purge ajusted deploy prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    purge: [
-        './src/pages/**/*.{js,jsx,ts,tsx}',
-        './src/components/**/*.{js,jsx,ts,tsx}'
-    ],
+    purge:['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
     content: [],
     theme: {
         extend: {},
