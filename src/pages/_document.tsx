79ee156dcdc9cec8c2403575c0df0c9fa38dd665
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-05-04 19:53:14 -0300

    ajustes eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head />
-            <body>
+            <body className="bg-black-500">
                 <Main />
                 <NextScript />
             </body>
